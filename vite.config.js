14f2eb9d542bbd144b842e1a61693c0535883a02
hemantvardani <hemant2vardani@gmail.com>
2024-05-03 19:40:55 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr()],
-  base: '/tic-tac-toe-vite/'
+  plugins: [react(), ssr()]
 }
