3e46b95d27fa82787797b778f72b4bc79544b6b7
hemantvardani <hemant2vardani@gmail.com>
2024-05-09 13:06:31 +0530

    .

--- renderer/PageShell.jsx
+++ renderer/PageShell.jsx
@@ -77,17 +77,4 @@ function Content({ children }) {
   )
 }
 
-function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10
-      }}
-    >
-      <a href="/">
-        <img src={logo} height={64} width={64} alt="logo" />
-      </a>
-    </div>
-  )
-}
+ 
--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -28,8 +28,50 @@ async function render(pageContext) {
         <meta charset="UTF-8" />
         <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="${desc}" />
-        <title>${`Tic-Tac-Toe`}</title>
+         <title>Challenge the Tic-Tac-Toe Master: Play Online for Free</title>
+
+        <meta name="description" content="Experience the Thrill of Tic-Tac-Toe! Play the Classic Game of X's and O's for Free at AwesomeGame.com. Challenge Your Friends or Take on the Computer. Start Your Ultimate Tic-Tac-Toe Adventure Now!">
+        <meta name="author" content="Neave Interactive">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+        <link rel="canonical" href="https://tictactoegame.in">
+
+        <!-- Set the page as mobile-friendly -->
+        <meta name="mobile-web-app-capable" content="yes">
+
+        <meta property="og:locale" content="en_US">
+
+        <meta name="keywords" content="Tic-Tac-Toe, Tic Tac Toe, X's and O's, online game, free game, multiplayer game">
+        <meta http-equiv="content-language" content="en">
+
+
+
+        <!-- Add Open Graph tags -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://tictactoegame.in">
+        <meta property="og:title" content="Tic-Tac-Toe - Play Tic-Tac-Toe online for free">
+        <meta property="og:description" content="Play the Tic-Tac-Toe game (Noughts and Crosses) for free online against Tic-Tac-Toe master or with your friends.">
+         <!-- Add Twitter Card tags -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="@tictactoe">
+        <meta name="twitter:title" content="Tic-Tac-Toe - Play Tic-Tac-Toe online for free">
+        <meta name="twitter:description" content="Play the Tic-Tac-Toe game (Noughts and Crosses) for free online against Tic-Tac-Toe master or with your friends.">
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         <style>
 @import url('https://fonts.googleapis.com/css2?family=Monoton&display=swap');
